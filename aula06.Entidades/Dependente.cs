ee376574dc8d0bffb1c07983c7a0d725989c9fc6
Aluno 04 <Aluno 04@904-04>
2017-08-05 14:44:46 -0300

    Criando o Projeto

---
+++ aula06.Entidades/Dependente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula06.Entidades
+{
+    public class Dependente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public Dependente()
+        {
+
+        }
+
+        public Dependente(int id, string nome, DateTime dataNascimento)
+        {
+            Id = id;
+            Nome = nome;
+            DataNascimento = dataNascimento;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"Id: {Id} Nome: {Nome} Data de Nascimento: {DataNascimento} ");
+        }
+    }
+}
---
+++ aula06.Entidades/Funcionario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula06.Entidades
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Salario { get; set; }
+        public DateTime DataAdmissao { get; set; }
+
+        public Funcionario()
+        {
+
+        }
+
+        public Funcionario(int id, string nome, decimal salario, DateTime dataAdmissao)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+            DataAdmissao = dataAdmissao;
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format($"Id: {Id}  Nome: {Nome}  Salario: {Salario} Admissão: {DataAdmissao}");
+        }
+
+        
+    }
+}
